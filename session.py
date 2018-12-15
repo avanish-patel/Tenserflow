6182830b1491839c38d84a177fa733b104847de1
Avanish Patel <patelabout@gmail.com>
2018-12-15 10:54:17 -0600

    Modified session

--- session.py
+++ session.py
@@ -1,19 +1,23 @@
 import tensorflow as tf
 
+# matrix 1 of 1 by 2
 matrix1 = tf.constant([[3,3]])
+# matrix 2 of 2 by 1
 matrix2 = tf.constant([[2],
 						[2]])
 
+print(matrix1)
+print(matrix2)
 
 product = tf.matmul(matrix1, matrix2) # np.dot(m1,m2)
 
-# way - 1
+# way - 1 to run session
 sess = tf.Session()
 result = sess.run(product)
 print(result)
 sess.close()
 
-# way - 2
+# way - 2 to run session
 with tf.Session() as sess:
 	result2 = sess.run(product)
 	print(result2)
